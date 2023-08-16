24b711939617d2eb304300f9dbbfa3acf449f53e
Joy Phoebe <joyapisi@gmail.com>
2023-08-16 13:45:11 +0300

    Add cors and fix api controller to use instance var and not local variable

--- app/controllers/api/greetings_controller.rb
+++ app/controllers/api/greetings_controller.rb
@@ -1,6 +1,6 @@
 class Api::GreetingsController < ApplicationController
   def random_greeting
-    random_message = Message.order(Arel.sql('RANDOM()')).first
-    render json: { greeting: random_message.message }
+    @random_message = Message.order('RANDOM()').first
+    render json: { greeting: @random_message.message }
   end
 end
